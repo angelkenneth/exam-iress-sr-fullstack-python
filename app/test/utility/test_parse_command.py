aa5a34fb830366e710378e57aed445924bf2f854
Angel Kenneth Tolentino <ghelo.ace@gmail.com>
2024-10-17 21:43:13 +0800

    added test_weird_commas

--- app/test/utility/test_parse_command.py
+++ app/test/utility/test_parse_command.py
@@ -14,3 +14,8 @@ def test_spaced_args_command():
 def test_wrapped_spaces_command():
     command_tuple = parse_command_line("  PLACE 1, 2, North  ")
     assert command_tuple == ("PLACE", ("1", "2", "North"))
+
+
+def test_weird_commas():
+    command_tuple = parse_command_line("  PLACE 1,, 2, ,, North  ")
+    assert command_tuple == ("PLACE", ("1", "2", "North"))
